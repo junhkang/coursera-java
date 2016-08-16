1f3ce436555bac92c19fd0355e264745f38dc72f
junhkang <kanhyuk@hanmail.net>
2016-08-16 16:03:34 -0500

    Caesarcipher
    encrypt msg with pushing random alphabets + word play

---
+++ CaesarCipher/CaesarCipher.java
@@ -0,0 +1,57 @@
+ 
+
+
+/**
+ * Write a description of CaesarCipher here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CaesarCipher {
+    private String alphabet;
+    private String shiftedAlphabet;
+    private int mainkey;
+    public CaesarCipher(int key){
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        shiftedAlphabet = alphabet.substring(key)+alphabet.substring(0,key);
+        mainkey = key;
+    }
+    //CaesarCipher cc = new CaesarCipher(mainkey);
+    
+    public String encrypt(String input){
+        CaesarCipher cc = new CaesarCipher(mainkey);
+        StringBuilder sb = new StringBuilder(input);
+        for (int i =0; i <sb.length(); i++){
+            char c = sb.charAt(i);
+            int idx = alphabet.indexOf(c);
+            if(idx!=-1){
+                c = shiftedAlphabet.charAt(idx);
+                sb.setCharAt(i, c);
+            }
+        }
+        return sb.toString();
+    }
+    public String decrypt(String input) {
+        CaesarCipher cc = new CaesarCipher(26-mainkey);
+        String input2 = cc.encrypt(input);
+        //StringBuilder sb = new StringBuilder(input2);
+        //for (int i =0; i <sb.length(); i++){
+       //     char c = sb.charAt(i);
+       //     int idx = alphabet.indexOf(c);
+       //     if(idx!=-1){
+       //         c = shiftedAlphabet.charAt(idx);
+       //         sb.setCharAt(i, c);
+       //     }
+       // }
+        return input2;
+    }
+    public void SimpleTest(){
+        //CaesarCipher cc = new CaesarCipher(mainkey);
+        String input = "I LOVE WHAT I LOVE";
+        String encinput = encrypt(input);
+        String original = decrypt(encinput);
+        System.out.println(encinput);
+       // CaesarCipher cc2 = new CaesarCipher(mainkey);
+        System.out.println(original);
+    }
+}
---
+++ CaesarCipher/WordPlay.java
@@ -0,0 +1,70 @@
+ 
+
+
+/**
+ * Write a description of WordPlay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.*;
+public class WordPlay {
+    public boolean isVowel(char ch){
+        char chl = Character.toLowerCase(ch);
+        //String chs = Character.toString(ch);
+        if (chl == 'a' || chl == 'i' || chl == 'u' || chl == 'o' || chl == 'e')   {
+            return true;
+        }
+        else return false;
+    }
+    public String replaceVowels(String phrase, char ch){
+        StringBuilder newstring = new StringBuilder(phrase);
+        int len = phrase.length();
+        for (int i = 0; i < len; i++){
+            String s = phrase.substring(i);
+            char cha = s.charAt(0);
+            //String chs = Character.toString(ch);
+            if (isVowel(cha)){
+                newstring.setCharAt(i, ch);
+            }
+        }
+        return newstring.toString();
+    }
+    public String emphasize(String phrase, char ch){
+        StringBuilder newstring = new StringBuilder(phrase);
+        int len = phrase.length();
+        for (int i = 0; i < len; i +=2){
+            String s = phrase.substring(i);
+            char cha = s.charAt(0);
+            if (cha == ch){
+                newstring.setCharAt(i, '*');
+            }
+        }
+        for (int i = 1; i < len; i+=2){
+            String s = phrase.substring(i);
+            char cha = s.charAt(0);
+            //String chs = Character.toString(ch);
+            if (isVowel(cha)){
+                newstring.setCharAt(i, '+');
+            }
+        }
+        return newstring.toString();
+    }
+    
+    public void replacetest(){
+        String test = "Hello World";
+        char testc = '*';
+        String k = replaceVowels(test, testc);
+        //System.out.println(k);
+        String test2 = "dna ctgaaactga";
+        char testc2 = 'a';
+        String kk = emphasize(test2, testc2);
+        System.out.println(kk);
+    }
+    public void testisvowel(){
+        char ch ='A';
+        boolean asdf = isVowel(ch);
+        System.out.println(asdf);
+    }
+}
